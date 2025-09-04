9b03f86b7cb69175ca73a632fc053977ef92893d
Poojitha <163494836+pooja123-2005@users.noreply.github.com>
2025-09-04 11:13:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        print("Form Submitted Successfully")
-        print("Registration Data: ", request.form)
-        return "Registration Successful"
-    return render_template('register.html')
+@app.route('/submit', methods=['POST'])
+def submit():
+    print("Form Submitted Successfully")
+    print("Registration Data: ", request.form)
+    return "Registration Successful"
 
 if __name__ == '__main__':
     app.run(debug=True)
